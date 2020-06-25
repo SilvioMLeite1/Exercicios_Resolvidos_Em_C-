be104ce54be1bf5d9f2388d34c18710792dee035
SilvioMLeite1 <smarioleite@outlook.com>
2020-06-25 01:28:23 -0300

    Problem Solution in C++ - URI 1001

---
+++ URI_1001/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    int x = 0;
+
+    cin>>a;
+    cin>>b;
+
+    x = a + b;
+
+    cout<<"X = "<< x << endl;
+
+    return 0;
+}
